b657d0f278de89c1304b1a519bc60eb7fda871b0
Jamie Redding <jamie@coldhands.dev>
2024-01-25 17:43:06 +0000

    Refactor EntryPoint out of Context

---
+++ src/main/java/dev/coldhands/pair/stairs/EntryPoint.java
@@ -0,0 +1,22 @@
+package dev.coldhands.pair.stairs;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class EntryPoint {
+
+    private final List<Pairing> historicalPairings;
+    private final List<String> availableDevelopers;
+    private final List<String> newJoiners;
+
+    public EntryPoint(List<Pairing> historicalPairings, List<String> availableDevelopers, List<String> newJoiners) {
+        this.historicalPairings = historicalPairings;
+        this.availableDevelopers = availableDevelopers;
+        this.newJoiners = newJoiners;
+    }
+
+    public List<ScoredPairCombination> computeScoredPairCombinations() {
+        DecideOMatic decideOMatic = new DecideOMatic(historicalPairings, new HashSet<>(availableDevelopers), new HashSet<>(newJoiners));
+        return decideOMatic.getScoredPairCombinations();
+    }
+}
