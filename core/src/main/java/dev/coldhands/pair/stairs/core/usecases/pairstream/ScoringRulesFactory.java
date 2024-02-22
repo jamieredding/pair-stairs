2f236346f8cb5c8ac7037d9dfd14533c0fcc359d
Jamie Redding <jamie@coldhands.dev>
2024-02-22 20:50:10 +0000

    Rename rule

--- core/src/main/java/dev/coldhands/pair/stairs/core/usecases/pairstream/ScoringRulesFactory.java
+++ core/src/main/java/dev/coldhands/pair/stairs/core/usecases/pairstream/ScoringRulesFactory.java
@@ -7,7 +7,7 @@ import dev.coldhands.pair.stairs.core.domain.pairstream.PairStreamCombination;
 import dev.coldhands.pair.stairs.core.usecases.pairstream.rules.MaintainStreamKnowledgeTransferRule;
 import dev.coldhands.pair.stairs.core.usecases.pairstream.rules.PenaliseEarlyContextSwitchingRule;
 import dev.coldhands.pair.stairs.core.usecases.pairstream.rules.PreferPairsOrStreamsThatHaveNotHappenedRecentlyRule;
-import dev.coldhands.pair.stairs.core.usecases.pairstream.rules.PreventConsecutivePairRepeatsRule;
+import dev.coldhands.pair.stairs.core.usecases.pairstream.rules.PreventConsecutiveDeveloperCombinationRepeatsRule;
 
 import java.util.List;
 
@@ -15,7 +15,7 @@ public final class ScoringRulesFactory {
 
     static List<ScoringRule<PairStreamCombination>> pairStreamScoringRules(CombinationHistoryRepository<PairStreamCombination> combinationHistoryRepository, PairStreamStatisticsService statisticsService) {
         return List.of(
-                new WeightedRule<>(200, new PreventConsecutivePairRepeatsRule(combinationHistoryRepository)),
+                new WeightedRule<>(200, new PreventConsecutiveDeveloperCombinationRepeatsRule(combinationHistoryRepository)),
                 new WeightedRule<>(100, new MaintainStreamKnowledgeTransferRule(combinationHistoryRepository)),
                 new WeightedRule<>(100, new PenaliseEarlyContextSwitchingRule(combinationHistoryRepository)),
                 new PreferPairsOrStreamsThatHaveNotHappenedRecentlyRule(statisticsService)
