c73585641a570d7178e2fd2e1d5ca5f000589e74
Jamie Redding <jamie@coldhands.dev>
2024-04-30 19:06:57 +0100

    Style ManualSelectionTable.tsx as a table

--- frontend/src/app/components/ManualSelectionTable.tsx
+++ frontend/src/app/components/ManualSelectionTable.tsx
@@ -1,6 +1,7 @@
-import {Button, Stack} from "@mui/material";
+import {Button, Stack, Table, TableBody, TableCell, TableContainer, TableRow} from "@mui/material";
 import {Close} from "@mui/icons-material";
 import PairStreamDto from "@/app/domain/PairStreamDto";
+import {Fragment} from "react";
 
 interface ManualSelectionTableProps {
     combination: PairStreamDto[]
@@ -8,18 +9,48 @@ interface ManualSelectionTableProps {
 }
 
 export default function ManualSelectionTable({combination, removeFromCombination}: ManualSelectionTableProps) {
+    const numberOfPairs = combination.length;
     return (
         <Stack gap={1}>
-            <div>Selected</div>
             {combination.length === 0 &&
                 <p>Make some pairs above</p>
             }
-            {combination.map(pair =>
-                <div>
-                    {pair.stream.displayName}, {pair.developers.map(dev => dev.displayName)}
-                    <Button onClick={() => removeFromCombination(pair)}><Close/></Button>
-                </div>
-            )}
+            {combination.length > 0 &&
+
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            {Array.from({length: numberOfPairs}, (_, pairIndex) => (
+                                <TableRow key={combination[pairIndex].stream.displayName}>
+                                    <TableCell colSpan={1}>
+                                        {combination[pairIndex].stream.displayName}
+                                    </TableCell>
+                                    {combination[pairIndex].developers.length === 2 &&
+                                        <Fragment>
+                                            <TableCell align="center">
+                                                {combination[pairIndex].developers[0].displayName}
+                                            </TableCell>
+                                            <TableCell align="center">
+                                                {combination[pairIndex].developers[1].displayName}
+                                            </TableCell>
+                                        </Fragment>
+                                    }
+                                    {combination[pairIndex].developers.length === 1 &&
+                                        <TableCell align="center" colSpan={2}>
+                                            {combination[pairIndex].developers[0].displayName}
+                                        </TableCell>
+                                    }
+                                    <TableCell align="right">
+                                        <Button
+                                            onClick={() => removeFromCombination(combination[pairIndex])}><Close/></Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            }
+
         </Stack>
     )
 }
