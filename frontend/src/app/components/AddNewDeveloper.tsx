6243d57905971dc782f2d0766321ea5ee79ca13b
Jamie Redding <jamie@coldhands.dev>
2024-05-01 08:32:50 +0100

    Configure AddNewDeveloper.tsx to do mutation

--- frontend/src/app/components/AddNewDeveloper.tsx
+++ frontend/src/app/components/AddNewDeveloper.tsx
@@ -2,20 +2,32 @@ import {Stack, TextField} from "@mui/material";
 import {useState} from "react";
 import SaveButton from "@/app/components/SaveButton";
 import ButtonRow from "@/app/components/ButtonRow";
+import {useAddDeveloper, useRefreshGetDeveloperInfo} from "@/app/infrastructure/DeveloperClient";
 
 interface AddNewDeveloperProps {
     onSubmit: () => void
 }
 
 export default function AddNewDeveloper({onSubmit}: AddNewDeveloperProps) {
-    const [name, setName] = useState<string>();
+    const [name, setName] = useState<string>("");
+    const {trigger} = useAddDeveloper()
+    const {refresh} = useRefreshGetDeveloperInfo()
+
+    function handleSubmit() {
+        const newName = name as string;
+        trigger({name: newName})
+            .then(async () => {
+                await refresh({id: -1, displayName: newName})
+                onSubmit();
+            })
+    }
 
     return (
         <Stack gap={1}>
             <TextField label="Name" variant="outlined" value={name}
                        onChange={(e) => setName(e.target.value)}/>
             <ButtonRow>
-                <SaveButton disabled={name === undefined || name.length === 0} onClick={onSubmit}/>
+                <SaveButton disabled={name.length === 0} onClick={handleSubmit}/>
             </ButtonRow>
         </Stack>
     )
--- frontend/src/app/infrastructure/DeveloperClient.ts
+++ frontend/src/app/infrastructure/DeveloperClient.ts
@@ -1,8 +1,12 @@
-import useSWR from "swr";
+import useSWR, {useSWRConfig} from "swr";
 import DeveloperInfoDto from "@/app/domain/DeveloperInfoDto";
+import useSWRMutation from "swr/mutation";
+import DeveloperDto from "@/app/domain/DeveloperDto";
+
+const DEVELOPER_INFO_PATH = "/api/v1/developers/info"
 
 export function useGetDeveloperInfos() {
-    const {data, error, isLoading} = useSWR("/api/v1/developers/info", getDeveloperInfos)
+    const {data, error, isLoading} = useSWR(DEVELOPER_INFO_PATH, getDeveloperInfos)
 
     return {
         allDevelopers: data,
@@ -11,7 +15,30 @@ export function useGetDeveloperInfos() {
     }
 }
 
-function getDeveloperInfos(url: string): Promise<DeveloperInfoDto[]> {
-    return fetch(url)
-        .then(res => res.json())
+export function useRefreshGetDeveloperInfo() {
+    const {mutate} = useSWRConfig()
+
+    const configuredMutation = (potentialNewDeveloperInfo: DeveloperInfoDto) =>
+        mutate(DEVELOPER_INFO_PATH, () => getDeveloperInfos(DEVELOPER_INFO_PATH), {
+            optimisticData: (existingDeveloperInfos) => [...existingDeveloperInfos, potentialNewDeveloperInfo],
+            rollbackOnError: true
+        })
+
+    return {refresh: configuredMutation};
+}
+
+export function useAddDeveloper() {
+    const {data, trigger, isMutating} = useSWRMutation("/api/v1/developers", addDeveloper);
+
+    return {trigger}
+}
+
+async function getDeveloperInfos(url: string): Promise<DeveloperInfoDto[]> {
+    const res = await fetch(url);
+    return await res.json();
+}
+
+function addDeveloper(url: string, {arg}: { arg: DeveloperDto }) {
+    return fetch(url, {method: "POST", body: JSON.stringify(arg)})
+    // todo response handling
 }
