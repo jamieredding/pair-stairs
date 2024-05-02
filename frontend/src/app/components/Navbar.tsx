21666f6fb82a9a13f8f806ed2dfa8346a24fe4a9
Jamie Redding <jamie@coldhands.dev>
2024-05-02 21:21:10 +0100

    Add simple navbar

---
+++ frontend/src/app/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import Toolbar from "@mui/material/Toolbar";
+import AppBar from "@mui/material/AppBar";
+import {Link, Stack} from "@mui/material";
+import {Fragment} from "react";
+
+interface NavItem {
+    displayText: string;
+    link: string;
+}
+
+const navItems: NavItem[] = [
+    {displayText: "Daily combination", link: "/daily-combination"},
+    {displayText: "Manual combination", link: "/manual-combination"},
+    {displayText: "Developers", link: "/developers"},
+    {displayText: "Streams", link: "/streams"},
+]
+
+export default function Navbar() {
+    return (
+        <AppBar>
+            <Toolbar>
+                <Stack direction="row" gap={2} alignItems="center">
+                    <Link variant="h4" href="/" sx={{color: '#fff'}} underline="none" pr={1}>
+                        pair-stairs
+                    </Link>
+                    {navItems.map((item, index) => (
+                        <Fragment key={index}>
+                            <Link
+                                key={item.link}
+                                sx={{color: '#fff'}}
+                                href={item.link}>
+                                {item.displayText}
+                            </Link>
+                        </Fragment>
+                    ))}
+                </Stack>
+            </Toolbar>
+        </AppBar>
+    )
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,24 @@
 import type {Metadata} from "next";
 import {inter} from "@/app/fonts";
 import "./globals.css";
+import Navbar from "@/app/components/Navbar";
 
 export const metadata: Metadata = {
-  title: "Pair Stairs",
-  description: "A tool for helping developers pair",
+    title: "Pair Stairs",
+    description: "A tool for helping developers pair",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <Navbar/>
+        {children}
+        </body>
+        </html>
+    );
 }
