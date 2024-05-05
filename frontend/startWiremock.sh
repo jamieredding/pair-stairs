876eb9ffa0b273f4787846f14d7de2fe04d6376c
Jamie Redding <jamie@coldhands.dev>
2024-05-05 11:35:24 +0100

    Add options to startWiremock.sh

--- frontend/startWiremock.sh
+++ frontend/startWiremock.sh
@@ -1,7 +1,35 @@
 #!/usr/bin/env bash
 
-docker run -it --rm \
-  -p 18082:8080 \
-  --name wiremock \
-  -v "$PWD/wiremock":/home/wiremock \
-  wiremock/wiremock:3.5.2 --verbose
+function devMode() {
+  docker run -it --rm \
+    -p 18082:8080 \
+    --name wiremock \
+    -v "$PWD/wiremock":/home/wiremock \
+    wiremock/wiremock:3.5.2 --verbose
+}
+
+function integrationMode() {
+  docker run -it --rm \
+    --name wiremock \
+    -v "$PWD/wiremock":/home/wiremock \
+    --network manual-pair-stairs \
+    wiremock/wiremock:3.5.2 --verbose --port 8081
+}
+
+function main() {
+  local mode="${1:?What mode do you want? dev or integration}"
+
+  case "${mode}" in
+    dev)
+      devMode
+      ;;
+    integration)
+      integrationMode
+      ;;
+    *)
+      echo "Usage ${0} {dev|integration}"
+      ;;
+  esac
+}
+
+main "$@"
