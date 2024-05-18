80fbfa05b2607ccc4e55cbf07be27bda88d37ee1
Jamie Redding <jamie@coldhands.dev>
2024-05-18 15:13:34 +0100

    Add scripts for dumping and restoring the mysql database

--- makefiles/backend.mk
+++ makefiles/backend.mk
@@ -1,7 +1,16 @@
 DB_CONTAINER_NAME=build_image__pair_stairs_db
+DATABASE_IMAGE_NAME=mysql:8.3.0
+MYSQL_HOST?=$(DB_CONTAINER_NAME)
+MYSQL_USER?=pair_stairs_user
+MYSQL_PASSWORD?=some-password
+MYSQL_ROOT_PASSWORD?=some-root-password
+MYSQL_NETWORK?=default
+MYSQL_PORT?=3306
+DUMP_FILE_PATH?=./all-databases.sql
+
 BACKEND_IMAGE_NAME=ghcr.io/jamieredding/pair-stairs-backend
 
-.PHONY: start-database wait-for-database run-db build-maven stop-database maven-release push-image-backend
+.PHONY: start-database dump-database restore-database dump-docker restore-docker wait-for-database run-db build-maven stop-database maven-release push-image-backend
 
 run-db: start-database wait-for-database
 
@@ -11,11 +20,11 @@ start-database:
 	@echo "Starting MySQL database container..."
 	@docker run --rm -d --name $(DB_CONTAINER_NAME) \
 	-e MYSQL_DATABASE=pair_stairs \
-	-e MYSQL_USER=pair_stairs_user \
-	-e MYSQL_PASSWORD=some-password \
-	-e MYSQL_ROOT_PASSWORD=some-root-password \
+	-e MYSQL_USER=$(MYSQL_USER) \
+	-e MYSQL_PASSWORD=$(MYSQL_PASSWORD) \
+	-e MYSQL_ROOT_PASSWORD=$(MYSQL_ROOT_PASSWORD) \
 	-p 3306:3306 \
-	mysql:8.3.0 || (echo "Failed to start MySQL container." ; exit 1)
+	$(DATABASE_IMAGE_NAME) || (echo "Failed to start MySQL container." ; exit 1)
 
 
 wait-for-database:
@@ -31,6 +40,33 @@ wait-for-database:
 		timeout=$$((timeout - 1)); \
 	done
 	@echo "MySQL database is ready!"
+
+dump-docker:
+	$(MAKE) dump-database MYSQL_HOST=docker-pair_stairs_db-1 MYSQL_NETWORK=docker_pair_stairs_net
+
+dump-database:
+	@echo "Dumping all databases from host $(MYSQL_HOST) on port $(MYSQL_PORT)..."
+	@docker run --rm --network $(MYSQL_NETWORK) $(DATABASE_IMAGE_NAME) sh -c '\
+		if mysqldump --all-databases -h$(MYSQL_HOST) -P$(MYSQL_PORT) -uroot -p"$(MYSQL_ROOT_PASSWORD)"; then \
+			echo "Dump successful" >&2; \
+		else \
+			echo "Dump failed" >&2; \
+			exit 1; \
+		fi' > $(DUMP_FILE_PATH) || { echo "Error: Failed to dump databases from host" >&2; exit 1; }
+
+restore-docker:
+	$(MAKE) restore-database MYSQL_HOST=docker-pair_stairs_db-1 MYSQL_NETWORK=docker_pair_stairs_net
+
+restore-database:
+	@echo "Restoring all databases to host $(MYSQL_HOST) on port $(MYSQL_PORT)..."
+	@docker run --rm --network $(MYSQL_NETWORK) -i $(DATABASE_IMAGE_NAME) sh -c '\
+		if mysql -h$(MYSQL_HOST) -P$(MYSQL_PORT) -uroot -p"$(MYSQL_ROOT_PASSWORD)"; then \
+			echo "Restore successful" >&2; \
+		else \
+			echo "Restore failed" >&2; \
+			exit 1; \
+		fi' < $(DUMP_FILE_PATH) || { echo "Error: Failed to restore databases to host" >&2; exit 1; }
+
 
 build-maven:
 	@echo "Running Maven build..."
