1589dcc9dda1a06a91f625288a5ae8b368f81cf7
Jamie Redding <jamie@coldhands.dev>
2025-08-04 19:04:36 +0100

    Add head content and favicon

--- new-frontend/src/routes/__root.tsx
+++ new-frontend/src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import {createRootRoute, Outlet} from '@tanstack/react-router'
+import {createRootRoute, HeadContent, Outlet} from '@tanstack/react-router'
 import {TanStackRouterDevtools} from '@tanstack/react-router-devtools'
 import RootContext from "../components/RootContext.tsx";
 import {CssBaseline} from "@mui/material";
@@ -6,14 +6,34 @@ import Navbar from "../components/Navbar.tsx";
 import Toolbar from "@mui/material/Toolbar";
 
 export const Route = createRootRoute({
+    head: () => ({
+        meta: [
+            {
+                name: "description",
+                content: "A tool for helping developers pair"
+            },
+            {
+                title: "Pair Stairs"
+            }
+        ],
+        links: [
+            {
+                rel: "icon",
+                href: "/favicon.ico"
+            }
+        ]
+    }),
     component: () => (
-        <RootContext>
-            <CssBaseline/>
-            <Navbar/>
-            {/*To pad the navbar above, see: https://github.com/mui/material-ui/issues/16844#issuecomment-517205129 */}
-            <Toolbar sx={{mb: 1}}/>
-            <Outlet/>
-            <TanStackRouterDevtools/>
-        </RootContext>
+        <>
+            <HeadContent/>
+            <RootContext>
+                <CssBaseline/>
+                <Navbar/>
+                {/*To pad the navbar above, see: https://github.com/mui/material-ui/issues/16844#issuecomment-517205129 */}
+                <Toolbar sx={{mb: 1}}/>
+                <Outlet/>
+                <TanStackRouterDevtools/>
+            </RootContext>
+        </>
     ),
 })
