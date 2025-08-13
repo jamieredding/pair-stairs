eb7f3a5395cccc428eed0a2ba8c68eb3ebe503ae
Jamie Redding <jamie@coldhands.dev>
2025-08-13 21:38:49 +0100

    Only redirect if spring was unable to find static content for a request
    Previously we were hiding other exceptions that were happening and potentially causing ourselves further problems

--- backend/src/main/java/dev/coldhands/pair/stairs/backend/infrastructure/web/controller/SinglePageApplicationDeepLinkErrorHandler.java
+++ backend/src/main/java/dev/coldhands/pair/stairs/backend/infrastructure/web/controller/SinglePageApplicationDeepLinkErrorHandler.java
@@ -2,12 +2,16 @@ package dev.coldhands.pair.stairs.backend.infrastructure.web.controller;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class SinglePageApplicationDeepLinkErrorHandler {
 
     @ExceptionHandler(Exception.class)
-    public String handleError() {
-        return "forward:/index.html";
+    public String handleError(Exception e) throws Exception {
+        if (e instanceof NoResourceFoundException) {
+            return "forward:/index.html";
+        }
+        throw e;
     }
 }
--- backend/src/test/java/dev/coldhands/pair/stairs/backend/infrastructure/web/controller/SinglePageApplicationDeepLinkErrorHandlerTest.java
+++ backend/src/test/java/dev/coldhands/pair/stairs/backend/infrastructure/web/controller/SinglePageApplicationDeepLinkErrorHandlerTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -20,5 +22,14 @@ class SinglePageApplicationDeepLinkErrorHandlerTest {
     void shouldReturnOkIfAPathIsRequestedThatIsNotSupported() throws Exception {
         mockMvc.perform(get("/does/not/exist"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldPropagateErrorsFromApiEndpoints() throws Exception {
+        mockMvc.perform(post("/api/v1/developers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("<notJson/>")
+        )
+                .andExpect(status().isBadRequest());
     }
 }
