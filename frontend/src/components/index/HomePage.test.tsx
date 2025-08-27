7b76d5d947ad4f98bdea04155253cd1360535055
Jamie Redding <jamie@coldhands.dev>
2025-08-27 21:40:09 +0100

    Delete test

--- frontend/src/components/index/HomePage.test.tsx
+++
@@ -1,9 +0,0 @@
-import {describe, it} from "vitest";
-import HomePage from "./HomePage";
-import {render} from "@testing-library/react";
-
-describe("HomePage", () => {
-    it("renders without crashing", () => {
-        render(<HomePage/>)
-    });
-});
