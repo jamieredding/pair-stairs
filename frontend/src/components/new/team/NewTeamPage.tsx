52972b36260d2f3a3403160db25c39ff0ab5333a
Jamie Redding <jamie@coldhands.dev>
2025-09-17 08:38:34 +0100

    Fix bug with slug TextField label overlapping with value when set programmatically
    This was due to the value initially starting as undefined, now we have a default

--- frontend/src/components/new/team/NewTeamPage.tsx
+++ frontend/src/components/new/team/NewTeamPage.tsx
@@ -1,13 +1,12 @@
-import {Card, CardContent, Stack, TextField, Typography} from "@mui/material";
+import {Card, CardContent, FormControl, Stack, TextField, Typography} from "@mui/material";
 import {useState} from "react";
-import {CustomLink} from "../../CustomLink.tsx";
 import ButtonRow from "../../ButtonRow.tsx";
 import SaveButton from "../../SaveButton.tsx";
 
 export function NewTeamPage() {
-    const [name, setName] = useState<string>()
-    const [slug, setSlug] = useState<string>()
-    const slugOrPlaceholder = slug || "tbc"
+    const [name, setName] = useState<string>("")
+    const [slug, setSlug] = useState<string>("")
+    const slugOrPlaceholder = formatSafeForSlug(slug || name || "tbc")
     const baseUrl = buildBaseUrl()
 
     return (
@@ -19,17 +18,24 @@ export function NewTeamPage() {
                         <div>
                             <TextField label="Name" variant="outlined" value={name}
                                        helperText="This is how your team name will appear in the UI"
-                                       onChange={(e) => setName(e.target.value)}/>
+                                       onChange={(e) => {
+                                           const value = e.target.value;
+                                           setName(value);
+                                           setSlug(formatSafeForSlug(value))
+                                       }}/>
                         </div>
                         <Stack direction="row" gap={1}>
-                            <TextField label="Slug" variant="outlined" helperText="This will be part of the url"
-                                       value={slug} onChange={(e) => setSlug(e.target.value)}/>
+                            <FormControl>
+                                <TextField label="Slug" variant="outlined"
+                                           helperText="This will be part of the url"
+                                           value={slug} onChange={(e) => {
+                                    setSlug(formatSafeForSlug(e.target.value));
+                                }}/>
+                            </FormControl>
                         </Stack>
                         <Stack direction="row" gap={1} alignItems="center">
-                            <Typography variant="body1">Your team's url will be:</Typography>
-                            <CustomLink to={`/team/$teamName`} params={{"teamName": slugOrPlaceholder}}>
-                                {baseUrl}/team/{slugOrPlaceholder}
-                            </CustomLink>
+                            <Typography variant="body1">Your team's url will
+                                be: {baseUrl}/team/{slugOrPlaceholder}</Typography>
                         </Stack>
                         <ButtonRow>
                             <SaveButton/>
@@ -44,4 +50,10 @@ export function NewTeamPage() {
 
 function buildBaseUrl() {
     return window.location.protocol + "//" + window.location.host
+}
+
+function formatSafeForSlug(potentialSlug: string): string {
+    return potentialSlug.toLowerCase()
+        .replaceAll(/[^a-z0-9-]/g, "-")
+    // todo what about multiple bad characters input, it shouldn't show multiple hyphens when generating (but maybe user is allowed?)
 }
