955cfeb5cb55197e095638abf4c9e05b96111b12
Jamie Redding <jamie@coldhands.dev>
2025-10-19 21:14:40 +0100

    Also use the requestBody as part of the cache key for calculate combinations
    Prior to this, we had a single cache for each page, irrelevant to what the requested devs and streams were. If you got page 1, then changed the devs, then requested page 1 again, you would see the combinations for the initial devs, rather than what you intended.

--- frontend/src/hooks/combinations/useCalculateCombinations.ts
+++ frontend/src/hooks/combinations/useCalculateCombinations.ts
@@ -1,13 +1,9 @@
-import useSWRInfinite, {type SWRInfiniteKeyLoader, type SWRInfiniteResponse} from "swr/infinite";
+import useSWRInfinite, {type SWRInfiniteResponse} from "swr/infinite";
 import type CalculateInputDto from "../../domain/CalculateInputDto.ts";
 import type ScoredCombinationDto from "../../domain/ScoredCombinationDto.ts";
 import {calculateCombinations} from "../../infrastructure/CombinationClient.ts";
 import type {ApiError} from "../../domain/ApiError.ts";
 import type PageDto from "../../domain/PageDto.ts";
-
-const getKey: SWRInfiniteKeyLoader = (pageIndex) => {
-    return `/api/v1/combinations/calculate?page=${pageIndex}&projection=page`
-}
 
 export default function useCalculateCombinations(requestBody: CalculateInputDto) {
     const {
@@ -16,7 +12,13 @@ export default function useCalculateCombinations(requestBody: CalculateInputDto)
         isLoading,
         setSize
     }: SWRInfiniteResponse<PageDto<ScoredCombinationDto>, ApiError> =
-        useSWRInfinite(getKey, (url: string) => calculateCombinations(url, {arg: requestBody}));
+        useSWRInfinite(
+            (pageIndex) => [
+                `/api/v1/combinations/calculate?page=${pageIndex}&projection=page`,
+                requestBody
+            ],
+            ([url]) => calculateCombinations(url, {arg: requestBody})
+        );
 
     return {
         combinationsPages: data,
